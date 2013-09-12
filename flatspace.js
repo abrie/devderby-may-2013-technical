dfa4fee9ccb3a40d17b7678adfdeb0d87a63420b
abrie <abrie@errante.local>
2013-09-12 15:37:07 -0400

    create boxview and add canvas to DOM

--- flatspace.js
+++ flatspace.js
@@ -1,11 +1,19 @@
 "use strict";
 
-define(['boxworld'], function(boxworld) {
+define(['boxworld', 'boxview'], function( boxworld, boxview ) {
 
-    var world = undefined;
+    var world, view = undefined;
 
     function initialize() {
         world = boxworld.create();
+
+        view = boxview.create( {
+            width:640,
+            height:480,
+            pixelsPerMeter:13
+        });
+
+        document.getElementById("flatspace").appendChild( view.canvas );
     }
 
     function update() {
